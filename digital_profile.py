fd2b508513059b91f7d145b7e1243946f0d9d41c
415-28-saengher <56759@yupparaj.ac.th>
2026-06-25 09:01:58 +0700

    Create digital_profile.py

---
+++ digital_profile.py
@@ -0,0 +1,12 @@
+ import streamlit as st
+
+st.title("My Digital Profile")
+
+st.header("แนะนำตัว")
+st.write("นายแสงเฮอ")
+st.write("ชื่อเล่น ใหม่")
+st.write("เลขที่28 ม.4/15")
+
+st.header("เป้าหมายและสิ่งที่อยากทำในอนาคต")
+st.write("เป้าหมาย: อยากเขียนโปรแกมเป้นทุกภาษา")
+st.write("สิ่งที่อยากเป็นในอนาคต: นักธุรกิจพันล้าน")
